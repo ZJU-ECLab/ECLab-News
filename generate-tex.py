25e4817e888e8d2c992e4736be8421b5804a5059
suisjaka <142398492+suisjaka@users.noreply.github.com>
2023-09-12 16:49:24 +0800

    Update generate-tex.py

--- generate-tex.py
+++ generate-tex.py
@@ -1,9 +1,9 @@
 from toml import load
 import os
 
-month = 7
+month = 8
 
-editor_in_chief = ["Qing Gu, Undergraduate, 2018", "Hongxu Zhou, Undergraduate, 2019"]
+editor_in_chief = ["Xuyao Fan, Undergraduate, 2021", "Jiaying Lu, Undergraduate, 2021"]
 
 editors = []
 
